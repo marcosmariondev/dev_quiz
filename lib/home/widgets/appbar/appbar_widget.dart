74e08d55c518aa9f31970e2ee57834d7399664e8
marcosmarion <marcosmariondev@gmail.com>
2021-04-21 23:33:44 -0300

    update widget

--- lib/home/widgets/appbar/appbar_widget.dart
+++ lib/home/widgets/appbar/appbar_widget.dart
@@ -1,5 +1,6 @@
 import 'package:dev_quiz/core/app_gradients.dart';
 import 'package:dev_quiz/core/app_text_styles.dart';
+import 'package:dev_quiz/home/widgets/score_card/score_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class AppBarWidget extends PreferredSize {
@@ -8,33 +9,43 @@ class AppBarWidget extends PreferredSize {
           preferredSize: Size.fromHeight(250),
           child: Container(
             height: 250,
-            decoration: BoxDecoration(gradient: AppGradients.linear),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text.rich(TextSpan(
-                      text: "Óla, ",
-                      style: AppTextStyles.title,
-                      children: [
-                        TextSpan(
-                          text: "marion dev",
-                          style: AppTextStyles.titleBold,
-                        )
-                      ])),
-                  Container(
-                    width: 58,
-                    height: 58,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      image: DecorationImage(
-                          image: NetworkImage(
-                              "https://avatars.githubusercontent.com/u/2903044?v=4")),
-                    ),
-                  )
-                ],
-              ),
+            child: Stack(
+              children: [
+                Container(
+                  height: 161,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(gradient: AppGradients.linear),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text.rich(TextSpan(
+                          text: "Óla, ",
+                          style: AppTextStyles.title,
+                          children: [
+                            TextSpan(
+                              text: "marion dev",
+                              style: AppTextStyles.titleBold,
+                            )
+                          ])),
+                      Container(
+                        width: 58,
+                        height: 58,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://avatars.githubusercontent.com/u/2903044?v=4")),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Align(
+                  alignment: Alignment(0.0, 1.0),
+                  child: ScoreCardWidget(),
+                )
+              ],
             ),
           ),
         );
