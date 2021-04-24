bee7658e0e5b6c607f5bf30b4f9a8befab193345
marcosmarion <marcosmariondev@gmail.com>
2021-04-24 20:42:58 -0300

    add answers'

---
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -0,0 +1,32 @@
+import 'package:dev_quiz/challenge/widgets/answer/answer_widget.dart';
+import 'package:dev_quiz/core/app_text_styles.dart';
+import 'package:flutter/material.dart';
+
+class QuizWidget extends StatelessWidget {
+  final String title;
+
+  const QuizWidget({Key? key, required this.title}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Text(
+            title,
+            style: AppTextStyles.heading15,
+          ),
+          SizedBox(
+            height: 24,
+          ),
+          AnswerWidget(
+              isRight: false,
+              isSelected: true,
+              title: "Teste de uma resposta Teste de uma resposta"),
+          AnswerWidget(title: "Teste de uma resposta Teste de uma resposta"),
+          AnswerWidget(title: "Teste de uma resposta Teste de uma resposta"),
+          AnswerWidget(title: "Teste de uma resposta Teste de uma resposta"),
+        ],
+      ),
+    );
+  }
+}
