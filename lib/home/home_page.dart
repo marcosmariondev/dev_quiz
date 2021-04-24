bdd8c88274fcd831acefc8983767d570bfd5d505
marcosmarion <marcosmariondev@gmail.com>
2021-04-24 15:48:29 -0300

    update home layout

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:dev_quiz/home/widgets/appbar/appbar_widget.dart';
 import 'package:dev_quiz/home/widgets/level_button/level_button_widget.dart';
+import 'package:dev_quiz/home/widgets/quiz_card/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,21 +15,43 @@ class _HomePageState extends State<HomePage> {
         appBar: AppBarWidget(),
         body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
             children: [
-              LevelButtonWidget(
-                label: "Fácil",
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  LevelButtonWidget(
+                    label: "Fácil",
+                  ),
+                  LevelButtonWidget(
+                    label: "Médio",
+                  ),
+                  LevelButtonWidget(
+                    label: "Difícil",
+                  ),
+                  LevelButtonWidget(
+                    label: "Perito",
+                  ),
+                ],
               ),
-              LevelButtonWidget(
-                label: "Médio",
+              SizedBox(
+                height: 20,
               ),
-              LevelButtonWidget(
-                label: "Difícil",
-              ),
-              LevelButtonWidget(
-                label: "Perito",
-              ),
+              Expanded(
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  children: [
+                    QuizCardWidget(),
+                    QuizCardWidget(),
+                    QuizCardWidget(),
+                    QuizCardWidget(),
+                    QuizCardWidget(),
+                    QuizCardWidget(),
+                  ],
+                ),
+              )
             ],
           ),
         ));
